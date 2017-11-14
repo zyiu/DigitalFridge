8a6379a96fcbdbe4027e53921a1bb37749fbe548
DebbiePao <dpao@berkeley.edu>
2017-11-13 19:23:30 -0800

    skeleton

---
+++ DigitalFridge/DigitalFridge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DigitalFridge
+//
+//  Created by Debbie Pao on 11/13/17.
+//  Copyright © 2017 Debbie Pao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
